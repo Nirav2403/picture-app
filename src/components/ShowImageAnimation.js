0814191d297c233c43257b1fd48701e5b7e7d340
nirav <nirav.prajapati2403@example.com>
2021-10-21 16:51:36 +0530

    update animation

--- src/components/ShowImageAnimation.js
+++ src/components/ShowImageAnimation.js
@@ -5,14 +5,12 @@ import { AlbumContext } from '../App';
 
 export const ShowImageAnimation = (props) => {
     const { album, dispatch } = useContext(AlbumContext);
-    console.log(props);
     if(album.length===0){
         props.history.push("/");
     }
     const [imgIndex, setIndex] = useState("")
-    const images = album[0].images;
+    let images = album[0].images;
     let leftValue = 30;
-    console.log(images);
     const waitToChange = (time) => {
         return new Promise(resolve => setInterval(() => resolve(""), time))
     }
@@ -30,17 +28,53 @@ export const ShowImageAnimation = (props) => {
         }
     }
 
+    // const imageRotate = async() => {
+    //     while(true){
+    //     document.getElementById("animation").style.backgroundImage = `url(${images[0]})`;
+    //     document.getElementById("image-silder").style.left = `${leftValue-10}rem`;
+    //     leftValue=leftValue-10;
+    //     await waitToChange(5000);
+    //     const newArray = images.shift();
+    //     images.push(newArray);
+    //     console.log("===========>",images);
+    //     }
+    // }
+
+    // const sequenceImage = () => {
+    //         return (
+    //             <div className={`sequence-image `} >
+    //                 <img src={images} alt="" className={`live-image ${images === 0 ? "active" : ""}`} />
+    //                 {/* <img src={image} alt="" className={`live-image ${imgIndex === index ? "active" : ""}`} /> */}
+    //                 <div className={`timing-line ${index === 0 ? "show-timeline" : ""}`}>
+    //                 {/* <div className={`timing-line ${imgIndex===index ? "show-timeline" : ""}`}> */}
+    //                     <div className={`timing-scroll`}></div>
+    //                 </div>
+    //             </div>
+    //         )
+    // }
     const sequenceImage = () => {
         return images.map((image, index) => {
             return (
-                <div className={`sequence-image ${imgIndex === index ? "active" : ""}`} ><img src={image} alt="" className="live-image" /></div>
+                <div className={`sequence-image `} >
+                    {/* <img src={image} alt="" className={`live-image ${index === 0 ? "active" : ""}`} /> */}
+                    <img src={image} alt="" className={`live-image ${imgIndex === index ? "active" : ""}`} />
+                    {/* <div className={`timing-line ${index === 0 ? "show-timeline" : ""}`}> */}
+                    <div className={`timing-line ${imgIndex===index ? "show-timeline" : ""}`}>
+                        <div className={`timing-scroll`}></div>
+                    </div>
+                </div>
             )
         })
     }
 
+        const imageMove = () => {
+
+        }
+
     useEffect(() => {
         imageLoop();
-    }, [])
+        // imageRotate(images);
+    },[])
 
     return (
         <div className="image-animation-container">
